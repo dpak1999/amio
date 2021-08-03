46fcfb8d2f5a18e32d390edb7b57c48672290fd3
dpak1999 <deepakkumardash313@gmail.com>
2021-08-03 17:52:35 +0530

    add : custom server

---
+++ server.js
@@ -0,0 +1,24 @@
+/** @format */
+
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const next = require("next");
+const dev = process.env.NODE_ENV !== "production";
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
+require("dotenv").config({ path: "./config.env" });
+const connectDB = require("./utilsServer/connectDb");
+const PORT = process.env.PORT;
+
+app.use(express.json());
+connectDB();
+
+nextApp.prepare().then(() => {
+  app.all("*", (req, res) => handle(req, res));
+
+  server.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(`Server running on ${PORT}`);
+  });
+});
