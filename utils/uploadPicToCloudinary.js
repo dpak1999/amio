7a99182dd42bd97839c9b028a74d1c73abc665dc
dpak1999 <deepakkumardash313@gmail.com>
2021-08-10 21:05:29 +0530

    add : cloudinary upload function

--- utils/uploadPicToCloudinary.js
+++ utils/uploadPicToCloudinary.js
@@ -1,1 +1,19 @@
 /** @format */
+
+import axios from "axios";
+
+const uploadPic = async (media) => {
+  try {
+    const form = new FormData();
+    form.append("file", media);
+    form.append("upload_preset", "amio_social");
+    form.append("cloud_name", "snazzycave");
+
+    const res = await axios.post(process.env.CLOUDINARY_URL, form);
+    return res.data.url;
+  } catch (error) {
+    return;
+  }
+};
+
+export default uploadPic;
