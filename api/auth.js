380d54a59b07afc32c5837eeb0becc8cd57c154d
dpak1999 <deepakkumardash313@gmail.com>
2021-08-11 17:27:04 +0530

    add : auth middleware and route

--- api/auth.js
+++ api/auth.js
@@ -6,6 +6,21 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const isEmail = require("validator/lib/isEmail");
 const UserModel = require("../models/UserModel");
+const FollowerModel = require("../models/FollowerModel");
+const authMiddleware = require("../middleware/authMiddleware");
+
+router.get("/", authMiddleware, async (req, res) => {
+  const { userId } = req;
+  try {
+    const user = await UserModel.findById(userId);
+    const userFollowerStats = await FollowerModel.findOne({ user: userId });
+
+    return res.status(200).json({ user, userFollowerStats });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Server Error");
+  }
+});
 
 router.post("/", async (req, res) => {
   const { email, password } = req.body.user;
